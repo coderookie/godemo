1002b36c5dac2744b8625f968ed149d93caccdce
zhanglei <zhanglei19881228@sina.com.cn>
2023-01-04 10:57:21 +0800

    leetcode

--- models/algorithm/combinationsum/README.md
+++ models/algorithm/combinationsum/README.md
@@ -1,4 +1,11 @@
 # 组合总和
 leetcode: https://leetcode.cn/problems/combination-sum/ <br />
 思路 <br />
-    1、
+    1、声明返回值的空二维int数组 <br />
+    2、循环目标数组, 当前值v1 <br />
+	3、当前值在数组内, append到返回值中 <br />
+    4、less(剩余值) = target - v1 <br />
+	5、剩余值在数组内, 则将less和v1, 放入返回值 <br />
+    6、递归, 将less和数组, 传入函数内, 如果结果不是空, 则循环递归的返回值, 将v1 append到递归的返回值中, 再放入返回值 <br />
+	7、递归的结束条件是: 1) 数组是空 2) target小于数组的最小值, 直接返回 <br />
+
--- models/algorithm/combinationsum/main.go
+++ models/algorithm/combinationsum/main.go
@@ -2,15 +2,87 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 )
 
-func combinationSum(candidates []int, target int) [][]int {
+func isIn(t int, candidates []int) bool {
+	for _, v := range candidates {
+		if t == v {
+			return true
+		}
+	}
 
+	return false
+}
+
+func combinationSum(candidates []int, target int) [][]int {
+	if len(candidates) == 0 {
+		return [][]int{}
+	}
+
+	// 先排序
+	sort.Ints(candidates)
+	if candidates[0] > target {
+		return [][]int{}
+	}
+
+	ret := make([][]int, 0)
+
+	for _, v := range candidates {
+		// 如果当前值等于target, 则把自己放入返回值
+		if v == target {
+			ret = append(ret, []int{v})
+		}
+
+		// 剩余值 = target - 当前值, 剩余值直接在数组中, 则放入返回值
+		t := make([]int, 0)
+		t = append(t, v)
+		less := target - v
+		if isIn(less, candidates) {
+			ret = append(ret, []int{v, less})
+		}
+
+		// 剩余值不在数组中, 则递归
+		r := combinationSum(candidates, less)
+		if len(r) > 0 {
+			for _, va := range r {
+				tm := make([]int, 0)
+				tm = append(tm, v)
+				tm = append(tm, va...)
+				ret = append(ret, tm)
+			}
+		}
+	}
+
+	// 一维数组排序
+	if len(ret) == 0 {
+		return [][]int{}
+	}
+
+	res := make([][]int, 0)
+	m := make(map[string]bool)
+	for _, v := range ret {
+		sort.Ints(v)
+		str := ""
+		for _, n := range v {
+			s := strconv.Itoa(n)
+			str += s
+		}
+		if _, ok := m[str]; !ok {
+			res = append(res, v)
+			m[str] = true
+		}
+	}
+
+	return res
 }
 
 func main() {
-	candidates := []int{2, 3, 6, 7}
-	target := 7
+	// candidates := []int{2, 3, 6, 7}
+	// target := 7
+	candidates := []int{8, 7, 4, 3}
+	target := 11
 	ret := combinationSum(candidates, target)
 	fmt.Println(ret)
 }
